df94adfa86a306afae687e1375e3590c74a913eb
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-12-06 15:56:28 -0700

    updates

--- src/Components/Api.jsx
+++ src/Components/Api.jsx
@@ -3,38 +3,50 @@ import { useState, useEffect } from 'react';
 const Api = () => {
 	const [quotes, setQuotes] = useState([])
 	const [counter, setCounter] = useState(0)
-	const [isLoading, setIsLoading] = useState(true)
- const [isError, setIsError] = useState(false)
-	const [user, setUser] = useState('Default User')
+	const [numQuotes, setNumQuotes] = useState(0)
 	const url = 'https://api.quotable.io/random?limit=3';
+
+
+
  useEffect(()=>{
-			fetch(url)
-			.then(response => {
-				if(response.status >= 200 && response.status < 300) {
-				return response.json();
-				} else {
-					throw new Error(response.statusText)
-				}
-			}).then(data=>{
+				fetch(`https://api.quotable.io/quotes/random?limit=${numQuotes}`)
+				.then(response => {
+					if(response.status >= 200 && response.status < 300) {
+						return response.json();
+					} else {
+						throw new Error(response.statusText)
+					}
+				}).then(data=>{
 				setQuotes(data);
+				console.log(data)
 		})}, [counter])
 
+
+
+		const [isLoading, setIsLoading] = useState(true)
+ 	const [isError, setIsError] = useState(false)
+		const [user, setUser] = useState('Default User')
+
+
 		useEffect(() => {
-  	fetch(url)
-  	.then((response) => {
-   	if(response.status >= 200 && response.status <= 299) {
-    	return response.json
-   	} else {
-    	setIsLoading(false)
-    	setIsError(true)
-    	throw new Error(response.statusText)
-   	}
-  	}).then((user) => {
-  		const {login} = user
-  		setUser(login)
-  		setIsLoading(false)
- 		}).catch((error) => {console.log(error)})
+  		fetch(url)
+  		.then((response) => {
+   		if(response.status >= 200 && response.status <= 299) {
+    		return response.json
+   		} else {
+    		setIsLoading(false)
+    		setIsError(true)
+    		throw new Error(response.statusText)
+   		}
+  		}).then((user) => {
+  			const {login} = user
+  			setUser(login)
+  			setIsLoading(false)
+ 			}).catch((error) => {console.log(error)})
 		}, [])
+
+
+
 
 		if(isLoading) {
   return (
@@ -53,13 +65,22 @@ const Api = () => {
 
 		return (
 				<div>
-					<h2 className='title'>{quotes.author}: </h2>
-					<h3 className="content">{quotes.content}</h3>
+					{
+						quotes.map((quotes) =>{
+							const {author, content} = quotes
+							return (
+								<>
+									<h2 className='title'>{author}: </h2>
+									<h3 className="content">{content}</h3>
+								</>
+							)
+						})
+					}
+					<input type="number" id='numQuotes' min='1' max='20' onChange={((e) => setNumQuotes(e.target.value))}/>
 					<button className="btn" onClick={()=>{setCounter(counter + 1)}}>ReGenerate</button>
 					<h4 className="counter">Number of Quotes Generated: {counter}</h4>
 				</div>
 		)
 }
 
-export default Api
-
+export default Api;
